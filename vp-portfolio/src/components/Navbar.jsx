b597d8ca9f9682d30ff4959757061fc623f9dca4
Viduna <122073048+VidunaPX@users.noreply.github.com>
2025-06-23 00:23:36 -0400

    Update Navbar.jsx

--- vp-portfolio/src/components/Navbar.jsx
+++ vp-portfolio/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <Link to="/interests"className ="navbar-links interests-link">Skills/Hobbies</Link>
         <Link to="/projects" className ="navbar-links home-link">Projects</Link>
         <Link to="/achievements" className ="navbar-links interests-link">Achievements</Link>
-        <Link to="/blog" className ="navbar-links interests-link">Blog</Link>
+        <Link to="/blog" className ="navbar-links home-link">Blog</Link>
       </ul>
     </nav>
   );
